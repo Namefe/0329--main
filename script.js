e358a87779bc2a9b22bc0a0b01c85192425b2fb8
namefe <smc9102@naver.com>
2025-04-05 17:46:41 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+const banner = document.querySelectorAll('.banner'); 
+const speed = 1;
+
+banner.forEach((img, index) => {
+    let position = index * 150; 
+    function moveImage() {
+        position += speed;
+        if (position > window.innerWidth) {
+            position = -650; 
+        }
+        img.style.left = position + 'px';
+        requestAnimationFrame(moveImage);
+    }
+    moveImage(); 
+});
+
+
+
+const wrapper = document.querySelector('.HeaderOpenBtn');
+wrapper.addEventListener('click',function(){
+    document.querySelector('headernavigation').style.display='block'
+})
+
+
+
+
+document.getElementById("btn1").addEventListener("click", function () {
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+  });
+  document.getElementById("btn2").addEventListener("click", function () {
+    document.getElementById("highlight").scrollIntoView({ behavior: "smooth" });
+  });
+  document.getElementById("btn3").addEventListener("click", function () {
+    document.getElementById("playlist").scrollIntoView({ behavior: "smooth" });
+  });
+  document.getElementById("btn4").addEventListener("click", function () {
+    document.getElementById("inviteYou").scrollIntoView({ behavior: "smooth" });
+  });
+
+  document.querySelector('.closebtn').addEventListener('click',()=>{
+    document.querySelector('.headernavigation').style.display='none'
+  })
+  document.querySelector('.HeaderOpenBtn').addEventListener('click',()=>{
+    document.querySelector('.headernavigation').style.display='block';
+  })
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const targets = document.querySelectorAll('.playliststicker-wrapper');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('on');
+        } else {
+          entry.target.classList.remove('on');
+        }
+      });
+    }, {
+      threshold: 0.5
+    });
+  
+    targets.forEach(target => observer.observe(target));
+  });
+
+  const box= document.querySelectorAll('.highlightHover');
+
+  box[0].style.backgroundColor = 'rgb(165,143,249)'
+  box[1].style.backgroundColor = 'rgb(238, 184, 229)'
+  box[2].style.backgroundColor = 'rgb(255, 133, 92)'
+  box[3].style.backgroundColor = 'rgb(247, 247, 247)'
+  box[4].style.backgroundColor = 'rgb(224, 245, 146)'
+
+
+  let playlistScrollArea = document.querySelector('.playlist-scrollArea');
+let playlistStickerWrapper = document.querySelector('.playlist-stickerWrapper');
+
+let scrollWidth = playlistScrollArea.scrollWidth;
+let verticalScrollHeight = playlistStickerWrapper.getBoundingClientRect().height-playlistScrollArea.getBoundingClientRect().height;
+
+document.addEventListener('scroll', horizontalScroll);
+function horizontalScroll(){
+
+
+    let stickyPosition = playlistScrollArea.getBoundingClientRect().top;
+    if(stickyPosition > 1){
+        return;
+    }else{
+        let scrolled = playlistStickerWrapper.getBoundingClientRect().top; //how much is scrolled?
+        playlistScrollArea.scrollLeft =(scrollWidth/verticalScrollHeight)*(-scrolled)*0.85;
+    
+    }
+}
